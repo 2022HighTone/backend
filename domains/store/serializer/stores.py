99a2641bd7bc650961a09a02eb234f720b89f663
이규락 <gyurak.lee@qualson.com>
2022-01-16 10:02:15 +0900

    feat add review

--- domains/store/serializer/stores.py
+++ domains/store/serializer/stores.py
@@ -5,10 +5,35 @@ from django.db.models import Q
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
-from domains.models import Store, Menu
+from domains.models import Store, Menu, Review
 
 
 User = get_user_model()
+
+
+class ReviewSeralizer(serializers.ModelSerializer):
+    username = serializers.CharField(source='user.username', read_only=True)
+    best_menu_name = serializers.CharField(source='menu.name', read_only=True)
+    menu = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = Review
+        fields = ('text', 'rating', 'menu', 'username', 'best_menu_name')
+
+    def create(self, validated_data):
+        user_id = self.context['request'].user.id
+        menu_name = validated_data.pop('menu')
+        
+        user = User.objects.get(id=user_id)
+        menu = Menu.objects.get(name=menu_name)
+        store = self.context['store']
+
+        return Review.objects.create(
+            user=user,
+            menu=menu,
+            store=store,
+            **validated_data
+        )
 
 
 class MenuSerializer(serializers.ModelSerializer):
@@ -23,12 +48,13 @@ class StoreSerializer(serializers.ModelSerializer):
     distance_name = serializers.CharField(source='distance.name')
     category_name = serializers.CharField(source='category.name')
     menus = serializers.SerializerMethodField()
+    reviews = serializers.SerializerMethodField()
 
     class Meta:
         model = Store
         fields = (
             'id', 'name', 'address', 'latitude', 'longitude', 'school', 'distance', 'category',
-            'school_name', 'distance_name', 'category_name', 'menus'
+            'school_name', 'distance_name', 'category_name', 'menus', 'reviews'
         )
 
     def create(self, validated_data):
@@ -54,12 +80,18 @@ class StoreSerializer(serializers.ModelSerializer):
     def get_menus(self, obj):
         print(self.context)
         price = self.context.pop('price')
-        if price is None:
+        if price is None or price == '':
             menus = Menu.objects.filter(store=obj)
         else:
             menus = Menu.objects.filter(
                 Q(store=obj) & Q(price__lte=price)
             )
         data = MenuSerializer(menus, many=True).data
+
+        return data
+
+    def get_reviews(self, obj):
+        reviews = Review.objects.filter(store=obj)
+        data = ReviewSeralizer(reviews, many=True).data
 
         return data
--- domains/urls.py
+++ domains/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 urlpatterns = [
     path('category/', include('domains.category.urls')),
     path('auth/', include('domains.auth.urls')),
-    path('school/', include('domains.school.urls'))
+    path('school/', include('domains.school.urls')),
+    path('store/', include('domains.store.urls'))
 ]
